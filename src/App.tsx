14533481aba1850dc0ba817e6547c0888c4b97be
tumoiakov <tumoyakov.work@outlook.com>
2025-09-03 23:29:08 +0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  // const [greetMsg, setGreetMsg] = useState("");
+  // const [name, setName] = useState("");
+
+  // async function greet() {
+  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  //   setGreetMsg(await invoke("greet", { name }));
+  // }
+
+  return (
+    <main className="container">
+      <h1>Welcome to Tauri + React</h1>
+    </main>
+  );
+}
+
+export default App;

e4b9f7561fec78f0309579db9d39e243768ffff8
tumoiakov <tumoyakov.work@outlook.com>
2025-09-06 19:04:13 +0700

    feat: setup libraries

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import "./global.css";
+import dayjs from "dayjs";
 
 function App() {
   // const [greetMsg, setGreetMsg] = useState("");
-  // const [name, setName] = useState("");
+  const [time, setTime] = useState("");
+  let timer: number | undefined;
 
   // async function greet() {
   //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   //   setGreetMsg(await invoke("greet", { name }));
   // }
 
+  useEffect(() => {
+    if (timer) {
+      clearInterval(timer);
+    }
+
+    timer = setInterval(() => {
+      const time = dayjs().format("HH:mm:ss");
+      setTime(time);
+    }, 1000);
+
+    return () => {
+      if (timer) {
+        clearInterval(timer);
+      }
+    };
+  }, []);
+
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
+      <h1>{time}</h1>
+      <div></div>
     </main>
   );
 }
